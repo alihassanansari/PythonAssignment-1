2c0a877672b64c7448d7cabd44733e3c965bf72d
Ali Hassan <ali.hassan.ansari@outlook.com>
2019-11-15 05:00:42 +0500

    Assignment 2 uploaded

---
+++ Assignment 1/1. Print Poem.py
@@ -0,0 +1,12 @@
+#########################
+#--- 1. Print Poem  ---#
+#########################
+
+line = []
+line.append("Twinkle, twinkle, little star,")
+line.append("\tHow I wonder what you are!")
+line.append("\t\tUp above the world so high,")
+line.append("\t\tLike a diamond in the sky.")
+line.append("Twinkle, twinkle, little star,")
+line.append("\tHow I wonder what you are")
+print (line[0], line[1], line[2], line[3], line[4], line[5], sep="\n")
